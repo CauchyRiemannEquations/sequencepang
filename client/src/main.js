4f1fe500182365e90c165dca68f03108abb29e11
Jihoon Oh <78549147+CauchyRiemannEquations@users.noreply.github.com>
2026-07-01 21:10:56 +0900

    Import rankingHome.css and initialize home ranking UI

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,10 @@
 import './style.css';
 import './style.lovable.css';
+import './rankingHome.css';
 import { initGameApp } from './gameEngine.js';
+import { initHomeRankingUI } from './rankingHome.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initGameApp();
+  initHomeRankingUI();
 });
